a271642e77e0c9e5f64f872cfa0b0b0d81a4d7fb
lgad31vn <lgad31vn@gmail.com>
2022-01-19 17:01:36 -0500

    options from users

--- atm-app/app.cpp
+++ atm-app/app.cpp
@@ -11,9 +11,31 @@ void showMenu() {
 
 
 int main() {
+    int option;
+    double balance = 500.00;
 
     cout << "ATM Application" << endl;
     showMenu();
+
+    cout << "what service do you want to perform? (1, 2, or 3)" << endl;
+    cin >> option;
+
+    switch(option) {
+        case 1: 
+            cout << "Your current balance is: $" << balance << endl;
+            break;
+
+        case 2: 
+            cout << "Your current balance: $" << balance << endl;
+            cout << "Deposit amount: ";
+            double amount = 0.00;
+            cin >> amount;
+            balance += amount;
+            cout << "You deposited $" << amount << endl;
+            cout << "Your current balance: $" << balance << endl;
+    }
+
+
 
     return 0;
 }
