6b4ea3f6a1e042cdac22f0dc83f0734e121def9d
lgad31vn <lgad31vn@gmail.com>
2022-01-19 16:51:39 -0500

    showMenu()

--- atm-app/app.cpp
+++ atm-app/app.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void showMenu() {
+    cout << "-----------MENU----------" <<endl;
+    cout << "|   1. Check Balance    |" << endl;
+    cout << "|   2. Deposit          |" << endl;
+    cout << "|   3. Withdraw         |" << endl;
+    cout << "-------------------------" <<endl;
+}
+
+
 int main() {
 
     cout << "ATM Application" << endl;
+    showMenu();
 
     return 0;
 }
