96015100a4b584257eca3e71f13cf3615ea37d19
lgad31vn <lgad31vn@gmail.com>
2022-01-19 16:18:47 -0500

    init demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // allow print out and get info from user
+#include <string> // works with string
+using namespace std; // 
+
+int main() {
+
+    // PRINTING
+    cout << "hello" <<endl;
+    cout << "this is Logan testing out C++" << endl;
+    
+
+    return 0;
+}
